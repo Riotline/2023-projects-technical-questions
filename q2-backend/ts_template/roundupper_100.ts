a6555d91652d632ae2b4ab1229bc3570fc630b08
Xander Gonzaga <z5366826@ad.unsw.edu.au>
2023-01-05 22:20:45 +1100

    q2_backend: partial submission p1/p2 for ts

--- q2-backend/ts_template/roundupper_100.ts
+++ q2-backend/ts_template/roundupper_100.ts
@@ -15,18 +15,53 @@ type spaceEntity =
 
 // === ADD YOUR CODE BELOW :D ===
 
+type lassoableAnimal = { type: spaceAnimal, location: location};
+
 // === ExpressJS setup + Server setup ===
 const spaceDatabase = [] as spaceEntity[];
 const app = express();
 
+app.use(express.json());
+
 // the POST /entity endpoint adds an entity to your global space database
 app.post('/entity', (req, res) => {
-    // TODO: fill me in
+    // Lacks input checks
+    if ("entities" in req.body) {
+        for (let entity of req.body["entities"]) {
+            spaceDatabase.push(entity);
+        }
+    }
+    console.log(spaceDatabase);
+    return res.sendStatus(200);
 });
 
 // lasooable returns all the space animals a space cowboy can lasso given their name
-app.get('/lassoable', (req, res) => {
-    // TODO: fill me in
+app.post('/lassoable', (req, res) => {
+    let cowboy = (spaceDatabase.find((e) => e.type === "space_cowboy" 
+                                         && e.metadata.name === req.body["cowboy_name"]));
+    let animals = {
+        space_animals: <lassoableAnimal[]>[],
+    };
+
+    if (cowboy === undefined)
+        return res.sendStatus(400);
+
+    let lassoLength = (cowboy!.metadata as spaceCowboy).lassoLength;
+
+    spaceDatabase.filter((e) => {
+        if (e.type !== "space_animal") return false;
+
+        // Normal distance calc
+        let dist = Math.abs(Math.sqrt(Math.pow(cowboy!.location.x - e.location.x, 2) + Math.pow(cowboy!.location.y - e.location.y, 2)));
+        return dist <= lassoLength;
+    }).forEach(e => {
+        animals.space_animals.push({
+            type: e.metadata as spaceAnimal,
+            location: e.location
+        });
+    })
+    
+    return res.send(animals);
 })
 
 app.listen(8080);
