3b5c870caa356595b3a228cc37838d7479bfc641
Xander Gonzaga <z5366826@ad.unsw.edu.au>
2023-01-05 19:29:41 +1100

    q1-frontend: submission

--- q1-frontend/components/Table/Table.tsx
+++ q1-frontend/components/Table/Table.tsx
@@ -45,7 +45,7 @@ export default function Table() {
       },
       {
         alert: 'Done!',
-        status: '<YOUR NAME>',
+        status: 'Riotline',
         updates: []
       }
     ]
@@ -67,7 +67,12 @@ export default function Table() {
               {content.status}
             </div>
             <div className={styles.item}>
-              {/* TODO: add updates */}
+              {content.updates.map((update) => 
+                <div className={styles.update}>
+                  {update.update}
+                  <div className={styles.date}>{update.date}</div>
+                </div>
+              )}
             </div>
           </div>
         ))}
